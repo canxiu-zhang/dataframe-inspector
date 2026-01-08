bab74be9d93bdb78b140062a1968648dc80831c0
Canxiu Zhang <canxiu_zhang@manulife.ca>
2026-01-08 10:13:12 -0500

    update version

--- dataframe_inspector/__init__.py
+++ dataframe_inspector/__init__.py
@@ -4,5 +4,5 @@ dataframe-inspector: Inspect nested structures in pandas DataFrame columns.
 
 from .inspector import Inspector
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["Inspector"]
